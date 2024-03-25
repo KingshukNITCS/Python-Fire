af15ef89cec9f36739edf90c1793236e9c1bbe03
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-03-25 07:18:26 +0530

    Create Strings.py

---
+++ Strings.py
@@ -0,0 +1,10 @@
+str = "abcde"
+rep_str = str*3
+print(rep_str)
+print(len(rep_str))
+print(str[1:3]) #bc
+print(str[1:])
+print(str[1:5])
+print(str[1:-2]) #bc
+print(str[-1]) #e
+print(str[:-1]) #abcd
