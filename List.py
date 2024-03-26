71d26daa61331c96a5ab38de33ed61d1aeb4cf7f
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-03-27 03:45:27 +0530

    Create List.py

---
+++ List.py
@@ -0,0 +1,10 @@
+# creating a student_info list to store the information about the student roll no,name, course and marks scored
+Student_info = [21, 'Rose', "CSE", 30, 40, 50]
+print(Student_info)
+
+
+
+Student_info = [21, 'Rose', "CSE", 30, 40, 50, 120]
+print("Student_roll :", Student_info[0], end=' , ')
+print("Student_name :", Student_info[1], end=', ')
+print("Student_totalMarks :", Student_info[-1], end=' ')
