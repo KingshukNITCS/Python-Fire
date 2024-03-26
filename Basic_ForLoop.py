95b3e75db00daeaa82b16c45b0ee8a7fc91886fa
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-03-26 23:57:34 +0530

    Update Basic_ForLoop.py

--- Basic_ForLoop.py
+++ Basic_ForLoop.py
@@ -20,3 +20,38 @@ word = "Python"
 for letter in word:
     print(letter)
   
+
+
+
+for i in range(5):
+    print(i,end=' ')
+
+
+
+for i in range(3):
+    for j in range(2):
+        print(f"({i}, {j})")
+	
+
+# Enumerate using For loop
+# The enumerate() function is handy for obtaining both the index and value during iteration: 
+# Output - 
+# Index: 0, Subj: Data-Science
+# Index: 1, Subj: Power BI
+# Index: 2, Subj: Python
+
+list1 = ["Data-Science", "Power BI", "Python"]
+
+for index, subject in enumerate(list1):
+    print(f"Index: {index}, Subj: {subject}")
+
+
+
+#List Comprehensions:
+squares = [x**2 for x in range(5)]
+print(squares)
+
+
+
+
+
