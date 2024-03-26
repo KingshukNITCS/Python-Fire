f93f2f6c1897748eb13e362ad6edfcea93ad8e39
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-03-26 23:52:42 +0530

    Create Basic_ForLoop.py

---
+++ Basic_ForLoop.py
@@ -0,0 +1,22 @@
+l = ["Data", "Science", "Python"]
+for i in l:
+	print(i)
+
+
+
+print("Dictionary Iteration")
+d = dict()
+
+d['key1'] = "val1"
+d['key2'] = 345
+for i in d:
+	print(i, d[i])
+
+
+
+#for loop with strings
+word = "Python"
+
+for letter in word:
+    print(letter)
+  
