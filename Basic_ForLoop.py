8ccf6231cfdd1a54ca4246df00c3ace5adc2491f
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-03-26 23:59:48 +0530

    Update Basic_ForLoop.py

--- Basic_ForLoop.py
+++ Basic_ForLoop.py
@@ -52,6 +52,20 @@ squares = [x**2 for x in range(5)]
 print(squares)
 
 
+# Armstrong Problem
 
+def is_armstrong_number(number):
+    num_str = str(number)
+    num_digits = len(num_str)
+    sum_of_digits = sum(int(digit) ** num_digits for digit in num_str)
+    return sum_of_digits == number
 
+# User input
+num = int(input("Enter a number: "))
 
+# Check and display the result
+if is_armstrong_number(num):
+    print(f"{num} is an Armstrong number.")
+else:
+    print(f"{num} is not an Armstrong number.")
+	
