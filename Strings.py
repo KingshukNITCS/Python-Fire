983b9fc81087c406f44042bd100262ae42abb8d1
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-03-26 23:34:16 +0530

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -19,3 +19,16 @@ print(stri.lstrip() + str)
 print(stri.rstrip() + str)
 print(str.replace("bcd","1245"))
 print(str.find("cd"))
+
+# Creating a String 
+String1 = "GeeksForGeeks"
+
+# Printing 3rd to 12th character 
+print("Slicing characters from 5-12: ") 
+print(String1[5:12]) 
+
+# Printing between 3rd and 2nd last character 
+print("Slicing characters between " +
+	"3rd and 3rd last character: ") 
+print(String1[3:-3]) 
+
