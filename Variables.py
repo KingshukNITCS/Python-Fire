fda97b70a45adafa0e71b15b53eb20281081e7f5
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-03-26 23:41:02 +0530

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -11,3 +11,28 @@ d = e = f = 20
 g = 8
 g += 2  # Equivalent to g = g + 2
 
+#some valid variables 
+variable = 120
+var_iab_le = 3.0
+_variable4 = 345.89
+_var_ = "string variable"
+Variable = 45
+_Var = 420
+
+
+x = 10
+y = 5
+x += y  #Equivalent to x = x + y
+print(x)
+
+x -= y  # Equivalent to x = x - y
+print(x)
+
+x *= 2  # Equivalent to x = x * 2
+print(x) 
+
+x /= 4  # Equivalent to x = x / 4
+print(x) 
+
+
+
