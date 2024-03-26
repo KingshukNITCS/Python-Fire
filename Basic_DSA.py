0565211362c2e0a9494d4e66219c5b17a26ea232
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-03-27 00:12:22 +0530

    Create Basic_DSA.py

---
+++ Basic_DSA.py
@@ -0,0 +1,23 @@
+# List Example
+items_list = ['apple', 'banana', 'orange', 5]
+
+# Tuple Example
+fruit_tuple = ('kiwi', 'grape', 'pear')
+
+
+# Dictionary Example
+person_info = {'Name': 'Sophie', 'Age': 28, 'Job': 'Scientist'}
+
+
+# Quick Queue: Deque
+# A deque is like a line where people can join or leave from both ends. This is handy when we need to manage information like a queue – first in, first out!
+
+
+from collections import deque
+
+# Creating a Deque
+people_line = deque(['Alice', 'Bob', 'Charlie'])
+
+# Joining and Leaving
+people_line.append('David')
+people_line.popleft()
