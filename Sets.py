d402fc062d7e739db98f7785d03b602112b40f92
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-04-03 13:38:02 +0530

    Update Sets.py

--- Sets.py
+++ Sets.py
@@ -31,3 +31,23 @@ difference of sets : {1, 9, 7}
 symmetric difference : {1, 2, 3, 6, 7, 9}
 '''
 
+Set = {2, 3, 4, 7, 8, 0}
+copy_set = Set.copy()
+set_length = len(Set)
+print("copy of set ", copy_set," set length ", set_length)
+Set.clear()
+print("Original set ", Set)
+'''
+Output - ('copy of set ', set([0, 2, 3, 4, 7, 8]), ' set length ', 6)
+
+('Original set ', set([]))
+'''
+
+
+Set = {2, 4, 5, 7, 8}
+if 5 in Set:
+    print("5 is in the set")
+'''
+in and not in: Checks for membership in a set.
+'''
+
