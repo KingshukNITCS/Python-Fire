8e342190cbf703e94ae9227511960da4cf54d114
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-04-03 21:36:47 +0530

    Create Libraries.py

---
+++ Libraries.py
@@ -0,0 +1,43 @@
+import math
+result = math.ceil(4.2)
+print(result)  
+
+
+ import math
+ result = math.floor(4.9)
+ print(result)
+
+import math
+result = math.trunc(4.9)
+print(result) 
+
+
+
+import math
+result = math.sqrt(16)
+print(result)
+
+
+import math
+angle = math.radians(45)  # Convert degrees to radians
+sin_result = math.sin(angle)
+cos_result = math.cos(angle)
+tan_result = math.tan(angle)
+print("sin_result :",sin_result, "cos_result :", cos_result, "tan_result :", tan_result)
+
+
+import math
+result = math.log(16, 2)
+print(result)  
+
+
+   import math
+   result = math.exp(2)
+   print(result)
+
+
+import math
+print(math.pi)
+print(math.e)
+
+
