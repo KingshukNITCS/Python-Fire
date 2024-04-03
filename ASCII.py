8d0a9f144692713ff739e0ae736c771160b9b231
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-04-04 04:56:55 +0530

    Create ASCII.py

---
+++ ASCII.py
@@ -0,0 +1,21 @@
+'''
+Character to ASCII Code
+You can use the ord function to get the ASCII code of a character.
+'''
+char = 'A'
+ascii_code = ord(char)
+print(f"The ASCII code of '{char}' is {ascii_code}")
+# Output - The ASCII code of 'A' is 65
+
+
+'''
+ASCII Code to Character
+Conversely, the chr
+ function converts an ASCII code to its corresponding character.
+ '''
+ascii_code = 97
+character = chr(ascii_code)
+print(f"The character with ASCII code {ascii_code} is '{character}'")
+# Output - The character with ASCII code 97 is 'a'
+
+
