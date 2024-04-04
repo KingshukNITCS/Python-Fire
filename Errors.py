332b253004b091a7a253834222f36c531d86e1e1
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-04-04 15:15:36 +0530

    Create Errors.py

---
+++ Errors.py
@@ -0,0 +1,28 @@
+# Syntax Error
+
+# initialize the amount variable 
+amount = 10000
+
+# check that You are eligible to 
+if(amount>2999) 
+	print("Item approved") 
+
+# Logical Errors
+total = 5 - 4# Logical Error
+print("Total is:", total)  # Expected output: Total is 9 (mistakenly prints 1)
+
+
+# Runtime Error
+result = 10 / 0 #ZeroDivisionError
+
+total = "5" + 5 #TypeError
+
+
+# IndexError
+my_list = [1, 2, 3]
+value = my_list[5]
+
+# ValueError
+age = int("twenty")
+
+
