364e1856122d32d55bf1bf216b72f3e5cf58438a
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-04-06 00:14:09 +0530

    Update Files_00.py

--- Files_00.py
+++ Files_00.py
@@ -114,3 +114,46 @@ Hello
 Output of Readlines function is 
 ['Hello \n', 'This is Delhi \n', 'This is Paris \n', 'This is London \n']
 '''
+
+
+
+
+# Python program to illustrate
+# Append vs write mode
+file1 = open("myfile.txt","w")
+L = ["This is Delhi \n","This is Paris \n","This is London \n"] 
+file1.writelines(L)
+file1.close()
+  
+# Append-adds at last
+file1 = open("myfile.txt","a")#append mode
+file1.write("Today \n")
+file1.close()
+  
+file1 = open("myfile.txt","r")
+print("Output of Readlines after appending") 
+print(file1.readlines())
+print()
+file1.close()
+  
+# Write-Overwrites
+file1 = open("myfile.txt","w")#write mode
+file1.write("Tomorrow \n")
+file1.close()
+  
+file1 = open("myfile.txt","r")
+print("Output of Readlines after writing") 
+print(file1.readlines())
+print()
+file1.close()
+
+
+'''
+Output:
+
+Output of Readlines after appending
+['This is Delhi \n', 'This is Paris \n', 'This is London \n', 'Today \n']
+
+Output of Readlines after writing
+['Tomorrow \n']
+'''
