da4223e1222507927da5a9f75ae298adb7c0e6e5
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-04-06 00:15:14 +0530

    Update Files_00.py

--- Files_00.py
+++ Files_00.py
@@ -22,6 +22,15 @@ close() function closes the file and frees the memory space acquired by that fil
 '''
 
 '''
+Read Only (‘r’) : Open text file for reading. The handle is positioned at the beginning of the file. If the file does not exists, raises the I/O error. This is also the default mode in which a file is opened.
+Read and Write (‘r+’): Open the file for reading and writing. The handle is positioned at the beginning of the file. Raises I/O error if the file does not exist.
+Write Only (‘w’) : Open the file for writing. For the existing files, the data is truncated and over-written. The handle is positioned at the beginning of the file. Creates the file if the file does not exist.
+Write and Read (‘w+’) : Open the file for reading and writing. For an existing file, data is truncated and over-written. The handle is positioned at the beginning of the file.
+Append Only (‘a’): Open the file for writing. The file is created if it does not exist. The handle is positioned at the end of the file. The data being written will be inserted at the end, after the existing data.
+Append and Read (‘a+’) : Open the file for reading and writing. The file is created if it does not exist. The handle is positioned at the end of the file. The data being written will be inserted at the end, after the existing data.
+'''
+
+'''
 Writing to a file
 
 There are two ways to write in a file.
