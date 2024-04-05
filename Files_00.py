d3ac5b78b559118ec6159a9d4de9c3eb0bf5d030
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-04-06 00:11:33 +0530

    Update Files_00.py

--- Files_00.py
+++ Files_00.py
@@ -43,3 +43,74 @@ readlines() : Reads all the lines and return them as each line a string element 
 Note: ‘\n’ is treated as a special character of two bytes 
 
 '''
+
+
+
+
+# Program to show various ways to read and
+# write data in a file.
+file1 = open("myfile.txt","w")
+L = ["This is Delhi \n","This is Paris \n","This is London \n"] 
+  
+# \n is placed to indicate EOL (End of Line)
+file1.write("Hello \n")
+file1.writelines(L)
+file1.close() #to change file access modes
+  
+file1 = open("myfile.txt","r+") 
+  
+print("Output of Read function is ")
+print(file1.read())
+print()
+  
+# seek(n) takes the file handle to the nth
+# bite from the beginning.
+file1.seek(0) 
+  
+print( "Output of Readline function is ")
+print(file1.readline()) 
+print()
+  
+file1.seek(0)
+  
+# To show difference between read and readline
+print("Output of Read(9) function is ") 
+print(file1.read(9))
+print()
+  
+file1.seek(0)
+  
+print("Output of Readline(9) function is ") 
+print(file1.readline(9))
+  
+file1.seek(0)
+# readlines function
+print("Output of Readlines function is ") 
+print(file1.readlines()) 
+print()
+file1.close()
+
+'''
+Output:
+
+Output of Read function is 
+Hello 
+This is Delhi 
+This is Paris 
+This is London 
+
+
+Output of Readline function is 
+Hello 
+
+
+Output of Read(9) function is 
+Hello 
+Th
+
+Output of Readline(9) function is 
+Hello 
+
+Output of Readlines function is 
+['Hello \n', 'This is Delhi \n', 'This is Paris \n', 'This is London \n']
+'''
