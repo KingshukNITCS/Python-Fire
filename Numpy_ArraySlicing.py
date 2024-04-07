06034e0301b06453f11795244b21dbf6f40971ba
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-04-07 16:33:47 +0530

    Create Numpy_ArraySlicing.py

---
+++ Numpy_ArraySlicing.py
@@ -0,0 +1,40 @@
+import numpy as np
+arr = np.array([1,2,3,4,5,6,7,8])
+arr[3: ]
+
+'''
+Output:
+
+array([4, 5, 6, 7, 8])
+'''
+
+print(arr[ : -3])
+print(arr[3  :  -3])
+
+'''
+Output:
+
+[1 2 3 4 5]
+[4 5]
+'''
+
+arr = np.array([[1,2,3,4],[5,6,7,8],[8,7,6,5],[4,3,2,1]])
+arr
+
+'''
+Output:
+
+array([[1, 2, 3, 4],
+       [5, 6, 7, 8],
+       [8, 7, 6, 5],
+       [4, 3, 2, 1]])
+'''
+# This is the 2D array which we will be using.
+print(arr[ 3 , 1])
+print()
+print(arr[ 1: , 1:])
+'''
+print(arr[ 3 , 1])
+print()
+print(arr[ 1: , 1:])
+'''
