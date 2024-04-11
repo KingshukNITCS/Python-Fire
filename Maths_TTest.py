0182524894287fba08daa446773c10b254bbb900
KingshukNITCS <87824297+KingshukNITCS@users.noreply.github.com>
2024-04-12 04:08:39 +0530

    Create Maths_TTest.py

---
+++ Maths_TTest.py
@@ -0,0 +1,10 @@
+'''
+A t-test is a type of inferential statistic used to determine if there is a significant difference between the means of two groups, which may be related in certain features.
+
+If t-value is large => the two groups belong to different groups. 
+
+If t-value is small => the two groups belong to same group.
+
+t-tests are widely used in the fields of research in hospitals by experts to gain important information about the medical data given to them about effects of various medicines and drugs on the population and helps them draw out important inferences regarding the same. However, it is the responsibility of the person to see to it that which t-test would bring out the best results and that all the assumptions of that t-test are adhered to.
+'''
+
